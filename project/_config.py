ae82e1c8e53667e582e4d47268dfa693d27bb357
Gary <garyherd@yahoo.com>
2015-04-16 01:48:55 -0500

    Add user authentication and database relationships

--- project/_config.py
+++ project/_config.py
@@ -3,8 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = \
     r'\x9b\x9b\x1b\x9b<\xbc\xd6kES\x8b\xb3\xa9\xa6\xf6x)*\x04%\xf30\x1e\xef'
--- project/forms.py
+++ project/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import Form
 from wtforms \
-    import TextField, DateField, IntegerField, SelectField, StringField
-from wtforms.validators import DataRequired
+    import TextField, DateField, IntegerField, SelectField, \
+    StringField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class AddTaskForm(Form):
@@ -20,3 +21,28 @@ class AddTaskForm(Form):
         ]
     )
     status = IntegerField('Status')
+
+
+class RegisterForm(Form):
+    name = StringField(
+        'Username', validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    email = StringField(
+        'Email', validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    password = PasswordField(
+        'Password', validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    confirm = PasswordField(
+        'Repeat Password',
+        [DataRequired(), EqualTo('password', message='Passwords must match')]
+    )
+
+
+class LoginForm(Form):
+    name = StringField(
+        'Username', validators=[DataRequired()]
+    )
+    password = PasswordField(
+        'Password', validators=[DataRequired()]
+    )

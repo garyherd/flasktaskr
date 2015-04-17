fb995c33853785f1dc23cf02b19bda8cfa4d309d
Gary <garyherd@yahoo.com>
2015-04-16 22:31:35 -0500

    add manage sessions. Chapter 10 complete!

--- project/views.py
+++ project/views.py
@@ -23,8 +23,6 @@ from models import Task, User
 # helper functions
 
 
-
-
 def login_required(test):
     @wraps(test)
     def wrap(*args, **kwargs):
@@ -41,6 +39,7 @@ def login_required(test):
 @app.route('/logout/')
 def logout():
     session.pop('logged_in', None)
+    session.pop('user_id', None)
     flash('Goodbye!')
     return redirect(url_for('login'))
 
@@ -53,6 +52,7 @@ def login():
             user = User.query.filter_by(name=request.form['name']).first()
             if user is not None and user.password == request.form['password']:
                 session['logged_in'] = True
+                session['user_id'] = user.id
                 flash('Welcome!')
                 return redirect(url_for('tasks'))
             else:
@@ -88,13 +88,16 @@ def new_task():
                 form.priority.data,
                 datetime.datetime.utcnow(),
                 '1',
-                '1'
+                session['user_id']
             )
             db.session.add(new_task)
             db.session.commit()
             flash('New entry was successfully posted. Thanks.')
-    return redirect(url_for('tasks'))
-
+            return redirect(url_for('tasks'))
+        else:
+            flash('All fields are required.')
+            return redirect(url_for('tasks'))
+    return render_template('tasks.html', form=form)
 
 # Mark tasks as complete
 @app.route('/complete/<int:task_id>/')

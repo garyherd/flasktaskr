1fdaea7a98a25eb96d16b80bc9c31bf2cf793d9f
Gary <garyherd@yahoo.com>
2015-04-18 15:55:54 -0500

    add error handling

--- project/forms.py
+++ project/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import Form
 from wtforms \
     import TextField, DateField, IntegerField, SelectField, \
     StringField, PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 class AddTaskForm(Form):
@@ -28,14 +28,14 @@ class RegisterForm(Form):
         'Username', validators=[DataRequired(), Length(min=6, max=25)]
     )
     email = StringField(
-        'Email', validators=[DataRequired(), Length(min=6, max=40)]
+        'Email', validators=[DataRequired(), Email(), Length(min=6, max=40)]
     )
     password = PasswordField(
         'Password', validators=[DataRequired(), Length(min=6, max=40)]
     )
     confirm = PasswordField(
         'Repeat Password',
-        [DataRequired(), EqualTo('password', message='Passwords must match')]
+        [DataRequired(), EqualTo('password')]
     )
 
 
--- project/views.py
+++ project/views.py
@@ -9,6 +9,7 @@ from flask import Flask, flash, redirect, render_template, \
 from forms import AddTaskForm, RegisterForm, LoginForm
 from flask.ext.sqlalchemy import SQLAlchemy
 import datetime
+from sqlalchemy.exc import IntegrityError
 
 
 ##################
@@ -32,6 +33,23 @@ def login_required(test):
             flash('You need to login first')
             return redirect(url_for('login'))
     return wrap
+
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field = %s" % (
+                getattr(form, field).label.text, error), 'error')
+
+
+def open_tasks():
+    return db.session.query(Task).filter_by(
+        status='1').order_by(Task.due_date.asc())
+
+
+def closed_tasks():
+    return db.session.query(Task).filter_by(
+        status='0'). order_by(Task.due_date.asc())
 
 
 # route handlers
@@ -64,21 +82,18 @@ def login():
 @app.route('/tasks/')
 @login_required
 def tasks():
-    open_tasks = db.session.query(Task).\
-        filter_by(status="1").order_by(Task.due_date.asc())
-    closed_tasks = db.session.query(Task) \
-        .filter_by(status="0").order_by(Task.due_date.asc())
     return render_template(
         'tasks.html',
         form=AddTaskForm(request.form),
-        open_tasks=open_tasks,
-        closed_tasks=closed_tasks
+        open_tasks=open_tasks(),
+        closed_tasks=closed_tasks()
     )
 
 # Add new tasks
 @app.route('/add/', methods=['GET', 'POST'])
 @login_required
 def new_task():
+    error = None
     form = AddTaskForm(request.form)
     if request.method == "POST":
         if form.validate_on_submit():
@@ -94,10 +109,14 @@ def new_task():
             db.session.commit()
             flash('New entry was successfully posted. Thanks.')
             return redirect(url_for('tasks'))
-        else:
-            flash('All fields are required.')
-            return redirect(url_for('tasks'))
-    return render_template('tasks.html', form=form)
+    return render_template(
+        'tasks.html',
+        form=form,
+        error=error,
+        open_tasks=open_tasks(),
+        closed_tasks=closed_tasks()
+    )
+
 
 # Mark tasks as complete
 @app.route('/complete/<int:task_id>/')
@@ -132,10 +151,14 @@ def register():
                 form.email.data,
                 form.password.data,
             )
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Thanks for registering. Please login.')
-            return redirect(url_for('login'))
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Thanks for registering. Please login.')
+                return redirect(url_for('login'))
+            except IntegrityError:
+                error = 'That username and/or email already exist.'
+                return render_template('register.html', form=form, error=error)
     return render_template('register.html', form=form, error=error)
 
 

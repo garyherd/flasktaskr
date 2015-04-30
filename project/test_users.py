f4ae58809b65d84ff9b5793c2969b21b19da96c7
Gary <garyherd@yahoo.com>
2015-04-29 20:56:13 -0500

    Chapter 13 complete

--- project/test_users.py
+++ project/test_users.py
@@ -11,7 +11,7 @@ TEST_DB = 'test.db'
 
 
 
-class AllTest(unittest.TestCase):
+class UserTest(unittest.TestCase):
 
     # helper methods
     def login(self, name, password):
@@ -128,68 +128,23 @@ class AllTest(unittest.TestCase):
         response = self.logout()
         self.assertNotIn(b'Goodbye!', response.data)
 
-    def test_logged_in_users_can_access_tasks_page(self):
-        self.register('Fletcher', 'fletcher@realpython.com', 'python101',
-                      'python101')
-        self.login('Fletcher', 'python101')
-        response = self.app.get('tasks/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Add a new task:', response.data)
+    def test_default_user_role(self):
 
-    # TODO: refactor this and above for DRY?
-    def test_not_logged_in_users_cannot_access_tasks(self):
-        response = self.app.get('tasks/', follow_redirects=True)
-        self.assertIn(b'You need to login first.', response.data)
-
-    def test_users_can_add_tasks(self):
-        self.create_user('Michael', 'michael@realpython.com', 'python')
-        self.login('Michael', 'python')
-        self.app.get('tasks/', follow_redirects=True)
-        response = self.create_task()
-        self.assertIn(
-            b'New entry was successfully posted. Thanks.', response.data
+        db.session.add(
+            User(
+                "Johnny",
+                "john@doe.com",
+                "johnny"
+            )
         )
 
-    def test_users_cannot_add_tasks_when_error(self):
-        self.create_user('Michael', 'michael@realpython.com', 'python')
-        self.login('Michael', 'python')
-        self.app.get('tasks/', follow_redirects=True)
-        response = self.app.post('add/', data=dict(
-            name='Go to the bank',
-            due_date='',
-            priority='1',
-            posted_date='02/04/2014',
-            status='1'
-        ), follow_redirects=True)
-        self.assertIn(b'This field is required.', response.data)
+        db.session.commit()
 
-    def test_users_can_complete_tasks(self):
-        self.create_user('Michael', 'michael@realpython.com', 'python')
-        self.login('Michael', 'python')
-        self.app.get('tasks/', follow_redirects=True)
-        self.create_task()
-        response = self.app.get("complete/1/", follow_redirects=True)
-        self.assertIn(b'The task is complete. Nice.', response.data)
+        users = db.session.query(User).all()
+        print users
+        for user in users:
+            self.assertEquals(user.role, 'user')
 
-    def test_users_can_delete_tasks(self):
-        self.create_user('Michael', 'michael@realpython.com', 'python')
-        self.login('Michael', 'python')
-        self.app.get('tasks/', follow_redirects=True)
-        self.create_task()
-        response = self.app.get("delete/1/", follow_redirects=True)
-        self.assertIn(b'The task was deleted.', response.data)
-
-    def test_users_cannot_complete_tasks_that_are_not_created_by_them(self):
-        self.create_user('Michael', 'michael@realpython.com', 'python')
-        self.login('Michael', 'python')
-        self.app.get('tasks/', follow_redirects=True)
-        self.create_task()
-        self.logout()
-        self.create_user('Fletcher', 'fletcher@realpython.com', 'python101')
-        self.login('Fletcher', 'python101')
-        self.app.get('tasks/', follow_redirects=True)
-        response = self.app.get("complete/1/", follow_redirects=True)
-        self.assertNotIn(b'The task is complete. Nice.', response.data)
 
 if __name__ == "__main__":
     unittest.main()

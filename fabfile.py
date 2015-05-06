affcad97a6a8efa6d07d6bc8a7ec69c2c9770f55
Gary <garyherd@yahoo.com>
2015-05-05 23:03:39 -0500

    Complete thru Ch15 Deployment options

--- fabfile.py
+++ fabfile.py
@@ -7,7 +7,7 @@ def test():
 
 def commit():
     message = raw_input("Enter a git commit message: ")
-    local("git commit -am {}".format(message))
+    local("git add . && git commit -am '{}'".format(message))
 
 
 def push():

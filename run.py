d47e6543e389be8007a1c03d7b515570e6240834
Gary <garyherd@yahoo.com>
2015-05-10 16:15:48 -0500

    Fix run.py to pick correct host

--- run.py
+++ run.py
@@ -3,8 +3,14 @@
 
 import os
 from project import app
+from project._config import DEBUG
 
-# port = int(os.environ.get('PORT', 5000))
-# app.run(host='0.0.0.0', port=port)
-app.run()
+host = '0.0.0.0'
+port = int(os.environ.get('PORT', 5000))
+
+if DEBUG == True:
+    app.run()
+else:
+    app.run(host=host, port=port)
+
 
